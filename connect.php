20a43fa816c5e6513d6812a0fcd54d4ca89b2e83
Kimeinei <kimiczarloma.salting@bicol-u.edu.ph>
2024-04-21 18:37:36 +0800

    hhehe 3306 tayo

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 
     //CREATE A CONNECTION TO THE DATABASE
-    $connection = mysqli_connect("localhost:4306", "root", "", "pabakal_db");
+    $connection = mysqli_connect("localhost:3306", "root", "", "pabakal_db");
 
     if(!$connection){
         die("Could not connect".mysqli_connect_error());
