599beba8e404d0344f75c9c03aa527364a8daf30
DyLex11 <danicalato.botin@bicol-u.edu.ph>
2024-05-13 20:50:18 +0800

    ratings

--- pabakalna-main3/home.php
+++ pabakalna-main3/home.php
@@ -9,11 +9,48 @@ if(!isset($user_id)){
 }
 
 if(isset($_POST['add_to_wishlist'])){
-   // Your existing code for adding to wishlist
+   
+   $product_id = $_POST['product_id'];
+   $product_name = $_POST['product_name'];
+   $product_price = $_POST['product_price'];
+   $product_image = $_POST['product_image'];
+   
+   $check_wishlist_numbers = mysqli_query($conn, "SELECT * FROM `wishlist` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
+
+   $check_cart_numbers = mysqli_query($conn, "SELECT * FROM `cart` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
+
+   if(mysqli_num_rows($check_wishlist_numbers) > 0){
+       $message[] = 'already added to wishlist';
+   }elseif(mysqli_num_rows($check_cart_numbers) > 0){
+       $message[] = 'already added to cart';
+   }else{
+       mysqli_query($conn, "INSERT INTO `wishlist`(user_id, pid, name, price, image) VALUES('$user_id', '$product_id', '$product_name', '$product_price', '$product_image')") or die('query failed');
+       $message[] = 'product added to wishlist';
+   }
 }
 
 if(isset($_POST['add_to_cart'])){
-   // Your existing code for adding to cart
+   $product_id = $_POST['product_id'];
+    $product_name = $_POST['product_name'];
+    $product_price = $_POST['product_price'];
+    $product_image = $_POST['product_image'];
+    $product_quantity = $_POST['product_quantity'];
+
+    $check_cart_numbers = mysqli_query($conn, "SELECT * FROM `cart` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
+
+    if(mysqli_num_rows($check_cart_numbers) > 0){
+        $message[] = 'already added to cart';
+    }else{
+
+        $check_wishlist_numbers = mysqli_query($conn, "SELECT * FROM `wishlist` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
+
+        if(mysqli_num_rows($check_wishlist_numbers) > 0){
+            mysqli_query($conn, "DELETE FROM `wishlist` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
+        }
+
+        mysqli_query($conn, "INSERT INTO `cart`(user_id, pid, name, price, quantity, image) VALUES('$user_id', '$product_id', '$product_name', '$product_price', '$product_quantity', '$product_image')") or die('query failed');
+        $message[] = 'product added to cart';
+    }
 }
 
 ?>
@@ -62,7 +99,7 @@ if(isset($_POST['add_to_cart'])){
 
    <div class="box-container">
 
-      <?php
+   <?php
          $select_products = mysqli_query($conn, "SELECT * FROM `products` LIMIT 9") or die('query failed');
          if(mysqli_num_rows($select_products) > 0){
             while($fetch_products = mysqli_fetch_assoc($select_products)){
@@ -78,41 +115,30 @@ if(isset($_POST['add_to_cart'])){
          <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
          <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
          <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
-         <input type="submit" value="add to wishlist" name="add_to_wishlist" class="option-btn">
-         <input type="submit" value="add to cart" name="add_to_cart" class="btn">
+         <input type="submit" value="Add to Wishlist" name="add_to_wishlist" class="option-btn">
+         <input type="submit" value="Add to Cart" name="add_to_cart" class="btn">
          
-         <!-- Reviews Section -->
-         <div class="reviews">
-            <h3>Product Reviews</h3>
+         <!-- Ratings Section -->
+         <div class="ratings">
+            <h3>Product Ratings</h3>
             <?php
                $product_id = $fetch_products['id'];
-               $reviews_query = mysqli_query($conn, "SELECT * FROM `reviews` WHERE product_id = '$product_id'");
-               if (mysqli_num_rows($reviews_query) > 0) {
-                  while ($review = mysqli_fetch_assoc($reviews_query)) {
+               $ratings_query = mysqli_query($conn, "SELECT AVG(rating) AS avg_rating FROM `reviews` WHERE product_id = '$product_id'");
+               $avg_rating = mysqli_fetch_assoc($ratings_query)['avg_rating'];
+               if ($avg_rating !== null) {
             ?>
-            <div class="review">
-               <p><strong>Rating:</strong> <?php echo $review['rating']; ?></p>
-               <p><strong>Comment:</strong> <?php echo $review['comment']; ?></p>
-            </div>
+            <p>Average Rating: <?php echo number_format($avg_rating, 1); ?></p>
             <?php
-                  }
                } else {
-                  echo '<p>No reviews yet.</p>';
+                  echo '<p>No ratings yet.</p>';
                }
             ?>
-            <!-- Add New Review Form -->
-            <form action="" method="post">
-               <input type="hidden" name="product_id" value="<?php echo $fetch_products['id']; ?>">
-               <input type="number" min="1" max="5" name="rating" placeholder="Rating (1-5)" required>
-               <textarea name="comment" placeholder="Enter your review" required></textarea>
-               <button type="submit" name="add_review">Add Review</button>
-            </form>
          </div>
       </div>
       <?php
             }
          } else {
-            echo '<p class="empty">no products added yet!</p>';
+            echo '<p class="empty">No products added yet!</p>';
          }
       ?>
 
